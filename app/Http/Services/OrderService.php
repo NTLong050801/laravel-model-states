3833e5ba647a6dd7cdd4ae366eff2b80536c73a4
nguyen thanh long <loglog050801>
2023-03-24 10:26:53 +0700

    log data

--- app/Http/Services/OrderService.php
+++ app/Http/Services/OrderService.php
@@ -6,28 +6,28 @@ use App\Models\LogState;
 use App\Models\Order;
 use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\Session;
-
+use Illuminate\Support\Facades\Log;
 class OrderService
 {
 
     public function show($id)
     {
-        return Order::where('id',$id)->first();
+        return Order::where('id', $id)->first();
     }
 
-    public function transition( $request)
+    public function transition($request)
     {
-      //  dd($request->state);
-       // dd($order->state);
+        //  dd($request->state);
+        // dd($order->state);
         //state tiep theo update
-            $order = Order::find($request->id);
-            //state hien tai arr
-            $status = $order-> state;
-            $comment = $request->input('comment');
+        $order = Order::find($request->id);
+        //state hien tai arr
+        $status = $order->state;
+        $comment = $request->input('comment');
 //            if($status == $request->state){
 //                dd("fail");
 //            }
-            $tran =  $status -> transitionTo($request->state);
+        $tran = $status->transitionTo($request->state);
         //dd($tran->state);
         LogState::create([
 
@@ -37,16 +37,22 @@ class OrderService
             'comment' => $comment,
             'created_at' => now()
         ]);
+        $data = "ID : ".$order->id." -- From state: ".$status." -- To state : ".$request->state;
+//        $file = fopen('../storage/app/history.text', 'w');
+//        fwrite($file, $data);
+//        fclose($file);
+        Log::info($data);
+
     }
 
     public function listLogStates($id)
     {
-         return  LogState::where("order_id",$id)->get();
+        return LogState::where("order_id", $id)->get();
     }
 
-    public function create( $request)
+    public function create($request)
     {
-       $order =  Order::create([
+        $order = Order::create([
             'name' => $request->name,
             'total' => $request->total
         ]);
